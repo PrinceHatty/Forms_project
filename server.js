b623f769d9c9b8f3376c5f54d33f95e3c00a13c0
Prince Hatty <72368504+PrinceHatty@users.noreply.github.com>
2022-04-13 09:06:23 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 const Nanoid = require("nanoid");
 
-MongoClient.connect("mongodb://localhost:27017/MyDb", function (err, db) {
-   
-     if(err) throw err;
-
-     //Write databse Insert/Update/Query code here..
-                
+MongoClient(process.env.MONGODB_URL, {
+  useNewUrlParser: true
 });
 
 console.log(`UUID with Nano ID sync: ${Nanoid.nanoid()}`);
