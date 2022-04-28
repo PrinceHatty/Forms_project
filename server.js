56bc94bec1804fc6cd663ba80aa6c5c8c1a44ed9
Prince Hatty <72368504+PrinceHatty@users.noreply.github.com>
2022-04-28 08:25:43 -0400

    e

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const Nanoid = require("nanoid");
 const fs = require("fs");
 
-let rawdata = fs.readFileSync("student-2.json");
-let users = JSON.parse(rawdata);
+// let rawdata = fs.readFileSync("student-2.json");
+// let users = JSON.parse(rawdata);
+// let user_data = JSON.stringify(users);
 
 // const file_writing_user_data = require("./User_data/forms.json");
-
-console.log(`UUID with Nano ID sync: ${Nanoid.nanoid()}`);
 
 const app = express();
 app.use(express.urlencoded());
@@ -31,24 +30,39 @@ app.get("/form_save", (req, res) => {
 });
 app.post("/form_save", (req, res) => {
   req.body.form_id = Nanoid.nanoid();
-  let data = JSON.stringify(req.body);
-  fs.writeFileSync("student-2.json", data);
-  console.log(req.body);
+  let data1 = JSON.stringify(req.body);
+  fs.readFile("student-2.json", function (err, data) {
+    var json = JSON.parse("forms" + data);
+    json.push(data1);
+    fs.writeFile("student-2.json", JSON.stringify(json), function (err) {
+      if (err) throw err;
+      console.log('The "data to append" was appended to file!');
+    });
+  });
   res.end();
 });
 
 //creating routes for viewing the form
 app.get("/:owner/d/:formid/view", (req, res) => {
+  // https://6scc2h-2000.sse.codesandbox.io/;asjnacpqwjpcsdjc/d/mwaOa759cATPXB0WCDLna/view
+
   let data = "";
 
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].form_owner_id === req.params.owner) {
-      if (users[i].form_id === req.params.owner) {
-        console.log("found!");
-        data += users[i];
+  // console.log(rawdata + "Raw data");
+
+  // console.log(users + "Users");
+  // console.log(user_data + "User_Data");
+  function hasValueDeep(json, findValue) {
+    const values = Object.values(json);
+    let hasValue = values.includes(findValue);
+    values.forEach(function (value) {
+      if (typeof value === "object") {
+        hasValue = hasValue || hasValueDeep(value, findValue);
       }
-    }
+    });
+    return hasValue;
   }
+  console.log(hasValueDeep(users, req.params.formid));
   res.render("view-from.ejs", {
     form_owner: req.params.owner,
     form_id: req.params.formid,
